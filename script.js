f958b40faf5c5b4a24e05e526ff73b3ba9e0a3c6
Brandon Raphael <brndnrphl@gmail.com>
2023-10-31 14:51:35 +0700

    removed useless shuffle function 	modified:   script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const moves = ['rock', 'paper', 'scissors'];
 
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1))
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
-
-
     // event listeners
     document.getElementById("rock").addEventListener("click", function () {
         handleUserSelection("rock");
